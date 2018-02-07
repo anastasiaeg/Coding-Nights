83ffa0b596a9b5c28b375d5a6cf63b70d31f947a
aegorov <aegorov@ncsu.edu>
2018-02-07 16:11:17 -0500

    Add challenge run

--- Coding Nights Planning/src/meeting7/Median.java
+++ Coding Nights Planning/src/meeting7/Median.java
@@ -2,6 +2,7 @@ package meeting7;
 
 import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.Scanner;
 
 /**
  * Find the median of random integers array
@@ -14,7 +15,7 @@ public class Median {
 		for (int i = 0; i < randomInts.length; i++) {
 			randomInts[i] = (int)(Math.random()*10000);
 		}
-		
+		fromDataStream();
 		int[] check = {0, 5, 8, 9, -2};
 		System.out.println("Median of array " + Arrays.toString(check) + "\nis " + medianEasy(check));
 		System.out.println("Median of array " + Arrays.toString(randomInts) + "\nis " + medianHard(randomInts));
@@ -55,4 +56,41 @@ public class Median {
 		return (double)(min.poll() + max.poll()) / 2;
 	}
 	
+	/**
+	 * Challenge run! 
+	 * @return
+	 */
+	public static void fromDataStream() {
+	    Scanner scanner = new Scanner( System.in );
+
+	    PriorityQueue<Integer> min = new PriorityQueue<Integer>((a,b)-> b - a);
+		PriorityQueue<Integer> max = new PriorityQueue<Integer>();
+	    System.out.print( "Type in an integer, if you want to exit, type any character: " );
+	    
+    	while (scanner.hasNextInt()) {
+    		int thing = scanner.nextInt();
+    		if (max.size() == 0) {
+    			max.add(thing);
+    			System.out.println("Median is " + thing);
+    		} else {
+    			if (thing < max.peek()) {
+    				min.add(thing);
+    				while (min.size() > max.size()) {
+    					max.add(min.poll());
+    				}
+    			} else {
+    				max.add(thing);
+    				while (max.size() > min.size()) {
+    					min.add(max.poll());
+    				}
+    			}
+    		}
+    		
+            System.out.println("Max: " + Arrays.toString(max.toArray()));
+            System.out.println("Min: " + Arrays.toString(min.toArray()));
+            System.out.println("Median is " + (max.size() > min.size()?max.peek(): (max.peek() + min.peek()) / 2.0));
+    	}
+
+    	scanner.close();
+	}
 }
